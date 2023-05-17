cd11e7dae20c1c74548a144136b0b43cbbf42f9c
Phạm Thị Thanh Bình <132878653+Ngocbinh227@users.noreply.github.com>
2023-05-17 11:37:34 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,50 +1,76 @@
-
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+body {background-color: powderblue;}
+h1   {color: blue;}
+p    {color: red;}
+</style>
+</head>
+</html>
+<html>
+    <body>
+        <h1> BÀI TẬP THỰC HÀNH TRÊN LỚP </h1>
+        <?php echo "1,"; ?>
 <?php
 //1. Xuất ra trình duyệt chuỗi ‘Hello World !’
 echo "Hello World!";
 ?>
 <?php echo "<br>" ?>
+<?php echo "2,"; ?>
 <?php
 //Xác định số phần tử của chuỗi 'HoangVanSang'
 echo strlen("HoangVanSang");
 ?>
 <?php echo "<br>" ?>
+<?php echo "3,"; ?>
 <?php 
 //3.Viết chương trình PHP để tính lập phương của một số nguyên.
 $x = 100;
 echo pow($x,3);
 ?>
 <?php echo "<br>" ?>
+<?php echo "4,"; ?>
 <?php
 //Viết chương trình PHP để đảo ngược một chuỗi.
 echo strrev("Hello Binh");
 ?>
-<?php echo "<br>" ?>
+    </body>
+</html>
+<html>
+    <body>
+        <h1> BÀI TẬP VỀ NHÀ </h1>
+<?php echo "1,"; ?>
 <?php
 //Viết một chương trình PHP để đếm số ký tự trong một chuỗi sử dụng hàm strlen().
 echo strlen("PHAM THI THANH BINH YEU NGUYEN NGOC DUY");
 ?>
 <?php echo "<br>" ?>
+<?php echo "2,"; ?>
 <?php
 //Viết một chương trình PHP để đếm số từ trong một chuỗi sử dụng hàm str_word_count().
 echo str_word_count("Pham Thi Thanh Binh");
 ?>
 <?php echo "<br>" ?>
+<?php echo "3,"; ?>
 <?php
 //Viết một chương trình PHP để đảo ngược một chuỗi sử dụng hàm strrev().
 echo strrev("duy yeu binh");
 ?>
 <?php echo "<br>" ?>
+<?php echo "4,"; ?>
 <?php
 //Viết một chương trình PHP để tìm kiếm một chuỗi con trong một chuỗi sử dụng hàm strpos().
 echo strpos("Binh yeu Duy","Duy");
 ?>
 <?php echo "<br>" ?>
+<?php echo "5,"; ?>
 <?php
 //Viết một chương trình PHP để thay thế một chuỗi con trong một chuỗi bằng một chuỗi khác sử dụng hàm str_replace().
 echo str_replace("Linh","Binh","Duy yeu Linh");
 ?>
 <?php echo "<br>" ?>
+<?php echo "6,"; ?>
 <?php
 //Viết một chương trình PHP để kiểm tra xem một chuỗi có bắt đầu bằng một chuỗi con khác không sử dụng hàm strncmp().
 function startsWith($x , $y) {
@@ -60,21 +86,25 @@ if (startsWith($x, $y)) {
 }
 ?>
 <?php echo "<br>" ?>
+<?php echo "7,"; ?>
 <?php
 //Viết một chương trình PHP để chuyển đổi một chuỗi thành chữ hoa sử dụng hàm strtoupper().
 echo strtoupper("binh");
 ?>
 <?php echo "<br>" ?>
+<?php echo "8"; ?>
 <?php
 //Viết một chương trình PHP để chuyển đổi một chuỗi thành chữ thường sử dụng hàm strtolower().
 echo strtolower("BINH");
 ?>
 <?php echo "<br>" ?>
+<?php echo "9,"; ?>
 <?php 
 //Viết một chương trình PHP để chuyển đổi một chuỗi thành chuỗi in hoa chữ cái đầu tiên của mỗi từ sử dụng hàm ucwords().
 echo ucwords("binh tinh cam");
 ?>
 <?php echo "<br>" ?>
+<?php echo "10,"; ?>
 <?php
 //Viết một chương trình PHP để loại bỏ khoảng trắng ở đầu và cuối chuỗi sử dụng hàm trim().
 function removeSpace($me){
@@ -86,6 +116,7 @@ echo $meAfter;
 ?>
 
 <?php echo "<br>" ?>
+<?php echo "11,"; ?>
 <?php
 //Viết một chương trình PHP để loại bỏ ký tự đầu tiên của một chuỗi sử dụng hàm ltrim().
 $str = "Hello World!";
@@ -104,6 +135,7 @@ echo $xAfter;
 ?>
 
 <?php echo "<br>" ?>
+<?php echo "12,"; ?>
 <?php
 //Viết một chương trình PHP để loại bỏ ký tự cuối cùng của một chuỗi sử dụng hàm rtrim().
 function removeLastCharacter($x) {
@@ -114,14 +146,23 @@ $xAfter = removeLastCharacter($x);
 echo $xAfter;
 ?>
 <?php echo "<br>" ?>
+<?php echo "13,"; ?>
 <?php
 //Viết một chương trình PHP để tách một chuỗi thành một mảng các phần tử sử dụng hàm explode().
+function splitString($string, $separator){
+    return explode( $separator, $string );
+}
+$string = "binh,ha,Duy";
+$separator = ",";
+$result = splitString($string, $separator);
+print_r($result); //hiển thị mảng kết quả 
 ?>
 <?php echo "<br>" ?>
+<?php echo "14,"; ?>
 <?php
 //Viết một chương trình PHP để nối các phần tử của một mảng thành một chuỗi sử dụng hàm implode().
 function connectElement($array, $separator){
-    return implode($separator, $array);
+    return implode($separator , $array);
 }
 $array = array("apple", "banana", "orange");
 $separator = ", ";
@@ -130,6 +171,7 @@ $result = connectElement($array, $separator);
 echo $result;
 ?>
 <?php echo "<br>" ?>
+<?php echo "15,"; ?>
 <?php
 //Viết một chương trình PHP để thêm một chuỗi vào đầu hoặc cuối của một chuỗi sử dụng hàm str_pad().
 function addStringToBeginning($string, $addition, $length) {
@@ -150,6 +192,7 @@ echo "Chuỗi sau khi thêm vào đầu: " . $newStringBeginning . "\n";
 echo "Chuỗi sau khi thêm vào cuối: " . $newStringEnd;
 ?>
 <?php echo "<br>" ?>
+<?php echo "16,"; ?>
 <?php
 //Viết một chương trình PHP để kiểm tra xem một chuỗi có kết thúc bằng một chuỗi con khác không sử dụng hàm strrchr().
 function endsWith($string, $ending) {
@@ -171,6 +214,7 @@ if (endsWith($string, $ending)) {
 ?>
 
 <?php echo "<br>" ?>
+<?php echo "17,"; ?>
 <?php
 //Viết một chương trình PHP để kiểm tra xem một chuỗi có chứa một chuỗi con khác không sử dụng hàm strstr().
 function containsSubstring($string, $substring) {
@@ -186,6 +230,7 @@ if (containsSubstring($string, $substring)) {
 }
 ?>
 <?php echo "<br>" ?>
+<?php echo "18,"; ?>
 <?php
 //Viết một chương trình PHP để thay thế tất cả các ký tự trong một chuỗi không phải là chữ cái hoặc số bằng một ký tự khác sử dụng hàm preg_replace().
 function replaceNonAlphanumeric($string, $replacement) {
@@ -193,7 +238,6 @@ function replaceNonAlphanumeric($string, $replacement) {
     return preg_replace($pattern, $replacement, $string);
 }
 
-// Ví dụ sử dụng
 $string = "Hello! @world123";
 $replacement = "-";
 $result = replaceNonAlphanumeric($string, $replacement);
@@ -201,6 +245,7 @@ $result = replaceNonAlphanumeric($string, $replacement);
 echo $result;
 ?>
 <?php echo "<br>" ?>
+<?php echo "19"; ?>
 <?php
 //Viết một chương trình PHP để kiểm tra xem một chuỗi có phải là một số nguyên hay không sử dụng hàm is_int().
 $x= 12.45;
@@ -212,6 +257,7 @@ if (is_int("$x"))
 }
 ?>
 <?php echo "<br>" ?>
+<?php echo "20,"; ?>
 <?php
 //Viết một chương trình PHP để kiểm tra xem một chuỗi có phải là một email hợp lệ hay không sử dụng hàm filter_var().
 $email = "phambinhvale@gmail.com";
@@ -221,3 +267,7 @@ if (filter_var("&email")){
     echo "This is not email.";
 }
 ?>
+
+    </body>
+</html>
+
